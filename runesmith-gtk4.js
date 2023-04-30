451bf785b50feae26dc79efa3ecf299243efee62
John Factotum <50942278+johnfactotum@users.noreply.github.com>
2023-04-30 08:38:37 +0000

    Use WebKitGTK 6.0

--- runesmith-gtk4.js
+++ runesmith-gtk4.js
@@ -3,7 +3,7 @@ import Gtk from 'gi://Gtk?version=4.0'
 import Gio from 'gi://Gio?version=2.0'
 import GLib from 'gi://GLib?version=2.0'
 import Adw from 'gi://Adw?version=1'
-import WebKit from 'gi://WebKit2?version=5.0'
+import WebKit from 'gi://WebKit?version=6.0'
 import Gdk from 'gi://Gdk'
 
 const appID = 'com.github.johnfactotum.Runesmith'
